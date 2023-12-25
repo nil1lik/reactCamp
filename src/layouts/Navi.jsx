2e34a8c1e9614c63a802173d2dd9afa2b2718926
Nil <144815514+nil1lik@users.noreply.github.com>
2023-12-25 23:56:52 +0300

    SignedIn ve SignedOut butonları aktifleştirildi.

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
 import CartSummary from "./CartSummary";
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  function handleSignOut() {
+    setIsAuthenticated(false)
+  }
+  function handleSignIn() {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -14,8 +23,8 @@ export default function Navi() {
 
           <Menu.Menu position="right">
             <CartSummary />
-            <SignedOut/>
-            <SignedIn/>
+            {isAuthenticated ? <SignedIn signOut={handleSignOut}/> 
+            : <SignedOut signIn ={handleSignIn}/>}
           </Menu.Menu>
         </Container>
       </Menu>
